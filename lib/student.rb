4bd89536d799c5813de5bd8c0f9dbf6e4bacbfb5
Austin  Burke <aburk3@gmail.com>
2018-11-16 22:13:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -66,11 +66,25 @@ class Student
 
   def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT COUNT(*)
+      SELECT *
       FROM students
-      WHERE grade = 9
+      WHERE grade = 9;
     SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
+  def self. students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade <= 11;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
