d6d0b7d3236a41e104459f8911c8568cd1b8fcae
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-24 13:05:50 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,28 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+  sql = "SELECT * FROM students"
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end 
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT name FROM students WHERE name = ? LIMIT 1"  
+    DB[:conn].execute(sql,name).map do |name|
+      self.new_from_db(name)
+      name.join 
+    end 
   end
   
   def save
@@ -35,6 +45,22 @@ class Student
 
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = "SELECT name FROM students WHERE grade = 9 "
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.students_below_12th_grade 
+    sql = "SELECT name FROM students WHERE grade < 11"
+    DB[:conn].execute(sql).join
+  end 
+  
+  def self.first_X_students_in_grade_10(x) 
+    sql = "SELECT name FROM students WHERE grade = 10"
+     #binding.pry
+    DB[:conn].execute(sql)
+  end 
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
