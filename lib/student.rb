baf32203fad39b90c7b66987fab526f7ca3f6335
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-25 01:03:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,11 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT name FROM students WHERE name = ? LIMIT 1"  
-    DB[:conn].execute(sql,name).map do |name|
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"  
+    selected_student = DB[:conn].execute(sql,name).map do |name|
       self.new_from_db(name)
-      name.join 
     end 
+    selected_student.first 
   end
   
   def save
@@ -52,18 +52,32 @@ class Student
   end 
   
   def self.students_below_12th_grade 
-    sql = "SELECT name FROM students WHERE grade < 11"
-    DB[:conn].execute(sql).join
+    sql = "SELECT * FROM students WHERE grade < '12' "
+     grab_student = DB[:conn].execute(sql)
+     grab_student.collect do |student|
+       new_from_db(student)
+     end 
   end 
   
   def self.first_X_students_in_grade_10(x) 
-    sql = "SELECT name FROM students WHERE grade = 10"
-     #binding.pry
-    DB[:conn].execute(sql)
+    sql = "SELECT * FROM students WHERE grade = '10' "
+    tenthgr_students = DB[:conn].execute(sql)
+    tenthgr_students.slice(0,x)
+  end 
+  
+  def self.first_student_in_grade_10 
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    first_student = DB[:conn].execute(sql) 
+      new_from_db(first_student[0]) 
   end 
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students "
+    DB[:conn].execute(sql)
+  end 
 end
