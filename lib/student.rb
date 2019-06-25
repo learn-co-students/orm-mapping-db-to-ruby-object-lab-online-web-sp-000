28a41763ea7dae36304bd36ad978c0a25fdbb22e
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 11:41:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -57,22 +57,42 @@ class Student
     end   
   end  
   
-  
-  
-  
-  
-def self.first_X_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(number)
          sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < 12
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+ 
+    DB[:conn].execute(sql,number).map do |row|
+      self.new_from_db(row)
+    end   
+  end    
+  
+   def self.first_student_in_grade_10
+         sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end  
+  
+  def self.all_students_in_grade_X(grade)
+         sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = grade
     SQL
  
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end   
-  end    
-  
+  end   
   
   def save
     sql = <<-SQL
