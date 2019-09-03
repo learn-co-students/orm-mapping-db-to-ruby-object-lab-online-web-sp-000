8479aa8e185ab39099817941fb8b1c48c9240e57
Lindsey Wells <github email address>
2019-09-03 15:21:02 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,12 +13,31 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
+  
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-  end
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+  end.first
+end
   
   def save
     sql = <<-SQL
@@ -45,4 +64,93 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-end
+
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9;
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT ?;
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+    self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end.first
+  end
+
+    def self.all_students_in_grade_X(x)
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?;
+      SQL
+  
+      DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+      end
+    end
+  end
+
+
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
