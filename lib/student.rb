32796bc5c86483700780f72bf653cb108b3c9089
Blythe Pollard <blytheepollard@gmail.com>
2019-11-10 17:31:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,15 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    
+   sql = <<-SQL 
+     SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+ 
+     DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
   
 end
