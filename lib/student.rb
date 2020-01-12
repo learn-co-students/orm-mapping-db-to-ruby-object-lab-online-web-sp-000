207c3729fa470f7d123e61edbbdbecae906d1fb3
Michael Carroll <carrollm2@gmail.com>
2020-01-12 00:45:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -69,11 +69,11 @@ class Student
   end
 
 
-  def self.first_X_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(number_of_students)
 
     sql = <<-SQL
       SELECT *
-      FROM students WHERE grade = ? LIMIT number
+      FROM students WHERE grade = 10 LIMIT #{number_of_students}
     SQL
 
     DB[:conn].execute(sql).map do |row|
@@ -82,7 +82,7 @@ class Student
   end
 
 
-  def self.first_student_in_grade_10(number)
+  def self.first_student_in_grade_10
 
     sql = <<-SQL
       SELECT *
@@ -99,7 +99,7 @@ class Student
 
     sql = <<-SQL
       SELECT *
-      FROM students WHERE grade = x
+      FROM students WHERE grade = #{x}
     SQL
 
     DB[:conn].execute(sql).map do |row|
