a9fcfd54fc2c0c50ea6386ae0279a12899b88808
sheriefCloud <29527317+sheriefCloud@users.noreply.github.com>
2020-02-13 16:01:08 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,20 +1,71 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    students = DB[:conn].execute(sql)
+    counter = 0
+    while counter < student.length do
+        self.new_from_db(students[counter])
+        binfindg.pry
+      # end
+    end
+   
+
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL 
+      SELECT students.id, students.name, students.grade 
+      FROM students 
+      WHERE students.name = name
+    SQL
+    student = DB[:conn].execute(sql)
+    self.new_from_db(student.flatten)
   end
   
+  def self.all_students_in_grade_9
+
+    sql = <<-SQL 
+      SELECT students.id, students.name, students.grade 
+      FROM students 
+      WHERE students.grade = 9
+    SQL
+    students = DB[:conn].execute(sql)
+    students
+  end
+
+  def self.students_below_12th_grade
+
+    sql = <<-SQL 
+      SELECT students.id, students.name, students.grade 
+      FROM students 
+      WHERE students.grade < 12
+    SQL
+    students = DB[:conn].execute(sql)
+    students
+    binding.pry
+  end
+
+
+
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
