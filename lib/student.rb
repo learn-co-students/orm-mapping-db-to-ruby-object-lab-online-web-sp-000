8c0ec31d99661858049a9f905e4c316003d3b036
Mark A Guza <guzamaguza@gmail.com>
2020-03-20 03:04:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -22,24 +22,55 @@ class Student
   end
 
   def self.all_students_in_grade_9
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE students.grade = 9
+    SQL
 
-
+    DB[:conn].execute(sql)
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE students.grade < 12
+    SQL
 
+    DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE students.grade = 10
+        LIMIT num
+    SQL
 
+    DB[:conn].execute(sql)
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE students.grade = 10
+        LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql)
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE students.grade = grade
+    SQL
 
+    DB[:conn].execute(sql)
   end
 
   def self.find_by_name(name)
