76cdb9ad9eb86593a496f66f3994d07a95778590
Ashley M Mader <ashley.mader12@gmail.com>
2020-04-08 17:54:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,21 +15,20 @@ class Student
     SQL
     
     DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
-  end
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * 
-      FROM students 
+      SELECT * FROM students 
       WHERE name = ?
       LIMIT 1 
     SQL
     
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
   
   def save
@@ -56,5 +55,30 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade <= 11
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    
   end
 end
