4291d8a4079fef15e8445e2e0341009272941f9e
cantstopcoding <jonathan_koss@yahoo.com>
2020-04-21 20:08:32 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,36 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM STUDENTS"
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM STUDENTS
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -39,5 +59,21 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_9
+    self.all.map {|s| s if s.grade == "9"}.reject {|ind| ind == nil}
+  end
+
+  def self.students_below_12th_grade
+    self.all.map {|s| s if s.grade.to_i < 12}.reject {|ind| ind == nil}
+  end
+
+  def self.first_X_students_in_grade_10(n)
+    # Return an array of n objects
+    # use map to return an array
+    # use self.new_from_db
+
+    # binding.pry
   end
 end
