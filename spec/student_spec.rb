bc8e899ca1c541dddef6c8c4db1e2d53c7554a19
McKayla Lankau <lankaukk@yahoo.com>
2021-01-27 21:36:33 -0500

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -130,27 +130,27 @@ describe Student do
       end
     end
 
-    # describe '.first_X_students_in_grade_10' do
-    #   it 'returns an array of the first X students in grade 10' do
+    describe '.first_X_students_in_grade_10' do
+      it 'returns an array of the first X students in grade 10' do
 
-    #     pat.name = "Pat"
-    #     pat.grade = 10
-    #     pat.save
-    #     sam.name = "Sam"
-    #     sam.grade = 10
-    #     sam.save
-    #     jess.name = "Jess"
-    #     jess.grade = 10
-    #     jess.save
+        pat.name = "Pat"
+        pat.grade = 10
+        pat.save
+        sam.name = "Sam"
+        sam.grade = 10
+        sam.save
+        jess.name = "Jess"
+        jess.grade = 10
+        jess.save
         
-    #     first_X_students = Student.first_X_students_in_grade_10(2)
-    #     expect(first_X_students.size).to eq(2), 'Requested first 2 students in grade 10. Expected Array of two elements'
-    #     expect(first_X_students.all? {|student| student.class == Student}).to eq(true), 'Expected Array of Student instances'
-    #     first_X_students = Student.first_X_students_in_grade_10(3)
-    #     expect(first_X_students.size).to eq(3), 'Requested first 3 students in grade 10. Expected Array of three elements'
-    #     expect(first_X_students.all? {|student| student.class == Student}).to eq(true), 'Expected Array of Student instances'
-    #   end
-    # end
+        first_X_students = Student.first_X_students_in_grade_10(2)
+        expect(first_X_students.size).to eq(2), 'Requested first 2 students in grade 10. Expected Array of two elements'
+        expect(first_X_students.all? {|student| student.class == Student}).to eq(true), 'Expected Array of Student instances'
+        first_X_students = Student.first_X_students_in_grade_10(3)
+        expect(first_X_students.size).to eq(3), 'Requested first 3 students in grade 10. Expected Array of three elements'
+        expect(first_X_students.all? {|student| student.class == Student}).to eq(true), 'Expected Array of Student instances'
+      end
+    end
 
     describe '.first_student_in_grade_10' do
       it 'returns the first student in grade 10' do
